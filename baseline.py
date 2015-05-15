2175bd2437f8cef2abd7ccac43635651abe565e5
Tianxin Zhao <tzhao@relcy.com>
2015-05-14 20:28:38 -0700

    -

--- baseline.py
+++ baseline.py
@@ -6,6 +6,8 @@ import data_utils.utils as du
 import data_utils.ner_base as nerbase
 from sklearn import linear_model
 
+train_file = 'data/train_small'
+dev_file = 'data/dev_small'
 
 def isCapital(x):
     if len(x) > 0:
@@ -39,13 +41,12 @@ tagnames = ["O", "B", "I", "L", "U"]
 num_to_tag = dict(enumerate(tagnames))
 tag_to_num = du.invert_dict(num_to_tag)
 wsize = 3
-X_train, y_train, words_train = du.generateData('data/train_small', word_to_num, tag_to_num, wsize)
+X_train, y_train, words_train = du.generateData(train_file, word_to_num, tag_to_num, wsize)
 
 print "Training..."
 
 TRAIN_SIZE = len(X_train) 
 X_s, y_s, w_s = dataSubset(TRAIN_SIZE, X_train, y_train, words_train)
-
 fv = extractFeatures(X_s, w_s, wsize, word_to_num)
 
 lr = linear_model.LogisticRegression(solver='lbfgs', multi_class='multinomial')
@@ -53,13 +54,9 @@ lr.fit(fv, y_s)
 y_pred = lr.predict(fv)
 print nerbase.full_report(y_s, y_pred, tagnames)
 
-X_dev, y_dev, words_dev = du.generateData('data/dev_small', word_to_num, tag_to_num, wsize)
-
-
+X_dev, y_dev, words_dev = du.generateData(dev_file, word_to_num, tag_to_num, wsize)
 DEV_SIZE = len(X_dev)
 Xd_s, yd_s, wd_s = dataSubset(DEV_SIZE, X_dev, y_dev, words_dev)
-
 fvdev = extractFeatures(Xd_s, wd_s, wsize, word_to_num)
 yp = lr.predict(fvdev)
-
-nerbase.full_report(yd_s, yp, tagnames)
+print nerbase.full_report(yd_s, yp, tagnames)
